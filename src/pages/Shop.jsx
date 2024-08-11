f7ed3d2ed4ae073bb6bfc7bbbee68f4cc12634d8
samx774 <121706488+samx774@users.noreply.github.com>
2024-08-11 07:18:55 +0300

    edit

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeadBanner from '../components/HeadBanner/HeadBanner'
-import Products from '../components/test/Test'
+import Products from '../components/Products/Products'
 
 export default function Shop() {
   return <>
